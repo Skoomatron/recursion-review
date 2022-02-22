e548e027ab6ec4d33da504ffa9471b6f88697bdd
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-02-22 17:45:28 -0500

    Work In Progress

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //base cases
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  }
+
+  if (obj === undefined) {
+    return 'null';
+  }
+
+
+  //array
+  var arrayResult = [];
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    for (var x = 0; x < obj.length; x++) {
+      arrayResult.push(stringifyJSON(obj[x]));
+    }
+    return '[' + arrayResult + ']';
+  }
+
+  //obj
+  var objResult = {};
+  if (typeof obj === 'object') {
+    if (obj === undefined) {
+      return '{}';
+    }
+    for (var key in obj) {
+      if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+        objResult[key] = stringifyJSON(obj[key]);
+      }
+    }
+    return '' + objResult;
+  }
+// testing
+
 };
